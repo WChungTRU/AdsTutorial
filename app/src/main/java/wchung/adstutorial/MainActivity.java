16b47dd0a8a0e445c0e9d103848dcd804f8e9a32
t00037035 <wchung@gmail.com>
2017-03-07 10:43:22 -0800

    second commit

--- app/src/main/java/wchung/adstutorial/MainActivity.java
+++ app/src/main/java/wchung/adstutorial/MainActivity.java
@@ -1,7 +1,10 @@
 package wchung.adstutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -9,7 +12,6 @@ import com.google.android.gms.ads.AdView;
 public class MainActivity extends AppCompatActivity {
 
     private AdView mAdView;
-    // ...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
